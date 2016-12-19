6311c7d01c3b656fe63d8c63b427bb978cb9b4fb
QING ZHOU <qczhou@ucdavis.edu>
2016-12-18 16:26:05 -0800

    update redis setting for prod

--- server/modules/redis.js
+++ server/modules/redis.js
@@ -1,4 +1,10 @@
 'use strict';
 var redis = require('redis');
-var client = redis.createClient();
-module.exports = client;
+if (process.env.REDISTOGO_URL) { //production
+  var rtg   = require("url").parse(process.env.REDISTOGO_URL);
+  var redis = require("redis").createClient(rtg.port, rtg.hostname);
+  redis.auth(rtg.auth.split(":")[1]);
+} else { //development
+  var redis = require("redis").createClient();
+}
+module.exports = redis;
